aafb0be22c80d2193d7a80909dcfb4587a3725d0
Sylvain Rayé <sly@diglin.com>
2013-05-22 01:45:18 +0200

    split template depending on the magento version, add missing template for use cases using persisten customer feature

--- app/code/community/Diglin/Username/Helper/Data.php
+++ app/code/community/Diglin/Username/Helper/Data.php
@@ -7,4 +7,13 @@
  * @copyright   Copyright (c) 2011-2013 Diglin (http://www.diglin.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Diglin_Username_Helper_Data extends Mage_Core_Helper_Abstract {}
+class Diglin_Username_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    public function isPersistentMustBeEnabled ()
+    {
+        return Mage::getStoreConfigFlag('username/general/enabled')
+            && Mage::helper('core')->isModuleEnabled('persistent')
+            && Mage::helper('core')->isModuleOutputEnabled('persistent')
+            && Mage::helper('persistent')->isEnabled();
+    }
+}
