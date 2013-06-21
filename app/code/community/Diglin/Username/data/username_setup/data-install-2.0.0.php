e9453ffbd48a5998638874106aad61a87dacf4c9
diglin <sylvain.raye@diglin.com>
2013-06-21 11:39:21 +0200

    generate correctly username for old customers while installing the module

--- app/code/community/Diglin/Username/data/username_setup/data-install-2.0.0.php
+++ app/code/community/Diglin/Username/data/username_setup/data-install-2.0.0.php
@@ -23,20 +23,21 @@ $select = $installer->getConnection()->select()
 $ids = $installer->getConnection()->fetchCol($select);
 
 $select = $installer->getConnection()->select()
-    ->from(array('c' => $this->getTable('customer_entity')), 'email');
+    ->from(array('c' => $this->getTable('customer_entity')), array('email', 'entity_id', 'entity_type_id'));
 
 if (!empty($ids)) {
 	$select->joinLeft(array('cev' => $this->getTable('customer_entity_varchar')), 'c.entity_id = cev.entity_id')
-	->where('cev.entity_id NOT IN ('. implode(',', $ids) . ')');
+	    ->where('cev.entity_id NOT IN ('. implode(',', $ids) . ')');
 }
 
 // Create username for old customers to prevent problem when creating an order as a guest
 $customers = $installer->getConnection()->fetchAll($select);
-foreach ($customers as $customer){
+foreach ($customers as $customer) {
     $customer['attribute_id'] = $usernameAttribute->getId();
     $email = $customer['email'];
     $pos = strpos($email, '@');
-    $customer['value'] = substr($email, 0, $pos) . substr(uniqid(), 0, 5);
+    $customer['value'] = substr($email, 0, $pos) . substr(uniqid(), 0, 5) . $customer['entity_id'];
+    
     unset($customer['email']);
     unset($customer['value_id']);
 
