1ca644195a7141e5f1845337beb0a904352995ae
Sylvain Rayé <sly@diglin.com>
2014-12-29 17:37:58 +0100

    Update Copyright

--- app/code/community/Diglin/Username/Helper/Data.php
+++ app/code/community/Diglin/Username/Helper/Data.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Diglin
+ * Diglin GmbH
  *
  * NOTICE OF LICENSE
  *
@@ -8,13 +8,11 @@
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
  * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
+
  *
  * @category    Diglin
  * @package     Diglin_Username
- * @copyright   Copyright (c) 2011-2014 Diglin (http://www.diglin.com)
+ * @copyright   Copyright (c) 2008-2015 Diglin GmbH - Switzerland (http://www.diglin.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
